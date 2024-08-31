854d9676b283e3e63758abff486d2c2067508550
Erfan Heshmati <heshmati74@gmail.com>
2024-08-31 14:39:15 +0330

    Add link to footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,14 +1,23 @@
 import { APP_NAME } from "@/lib/constants";
+import Link from "next/link";
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <footer className="border-t">
-      <div className="p-5 flex-center text-center text-sm">
+      <div className="p-5 text-center text-sm">
         &copy; {year} {APP_NAME} | All Rights reserved.
         <br />
-        Developed by Erfan Heshmati
+        <span>
+          Developed by{" "}
+          <Link
+            href="https://erfanweb.vercel.app/"
+            className="hover:text-muted-foreground"
+          >
+            Erfan Heshmati
+          </Link>
+        </span>
       </div>
     </footer>
   );
