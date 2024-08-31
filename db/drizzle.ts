c9a99c2acf07c6ce6c6f8d54f319763d8a434792
Erfan Heshmati <heshmati74@gmail.com>
2024-08-31 13:58:59 +0330

    Load products from database

---
+++ db/drizzle.ts
@@ -0,0 +1,10 @@
+import * as schema from "./schema";
+
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+
+const db = drizzle(sql, {
+  schema,
+});
+
+export default db;
