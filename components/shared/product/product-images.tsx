42407bba9fbb46b6744e76703052fb5cf5fd4b4b
Erfan Heshmati <heshmati74@gmail.com>
2024-08-31 19:25:15 +0330

    Create product details page

---
+++ components/shared/product/product-images.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export default function ProductImages({ images }: { images: string[] }) {
+  const [current, setCurrent] = React.useState(0);
+
+  return (
+    <div className="space-y-4">
+      <Image
+        src={images[current]}
+        alt="product image"
+        width={1000}
+        height={1000}
+        className="min-h-[300px] object-cover object-center rounded-sm"
+      />
+      <div className="flex">
+        {images.map((image, index) => (
+          <div
+            key={image}
+            className={cn(
+              "border mr-2 cursor-pointer rounded-sm hover:border-orange-600",
+              current === index && "border-orange-500"
+            )}
+            onClick={() => setCurrent(index)}
+          >
+            <Image
+              src={image}
+              alt={"image"}
+              width={100}
+              height={100}
+              className="rounded-sm"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) => {
+  const stringValue = value.toString();
+  const [intValue, floatValue] = stringValue.includes(".")
+    ? stringValue.split(".")
+    : [stringValue, ""];
+
+  return (
+    <p className={cn("text-xl", className)}>
+      <span className="text-sm align-super">$</span>
+      {intValue}
+      <span className="text-sm align-super">{floatValue}</span>
+    </p>
+  );
+};
+
+export default ProductPrice;
